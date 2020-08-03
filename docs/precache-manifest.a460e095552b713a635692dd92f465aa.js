bc34228864a9b2b7de83560b8f5e8cb62e8647ab
Omar Oliver <o.oliver.r@live.com.mx>
2020-08-03 16:45:58 -0500

    Docs added

---
+++ docs/precache-manifest.a460e095552b713a635692dd92f465aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78f0a93ad3540b0aa3bf9066ad849132",
+    "url": "/index.html"
+  },
+  {
+    "revision": "028ec05bdbe2dd0aec6b",
+    "url": "/static/css/main.ff3e27cb.chunk.css"
+  },
+  {
+    "revision": "89410fccec1793ce5168",
+    "url": "/static/js/2.30d82008.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.30d82008.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "028ec05bdbe2dd0aec6b",
+    "url": "/static/js/main.6f1473fc.chunk.js"
+  },
+  {
+    "revision": "6a26a6a45f9e65ecd26f",
+    "url": "/static/js/runtime-main.0ba3af82.js"
+  }
+]);
